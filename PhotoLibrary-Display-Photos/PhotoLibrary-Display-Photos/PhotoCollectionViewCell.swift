646c4bbaba49899e420f1c5b425a27275e1a80c6
Mohammad Azam <azamsharp@gmail.com>
2019-02-17 18:37:23 -0600

    Displaying Photo Library in iOS

---
+++ PhotoLibrary-Display-Photos/PhotoLibrary-Display-Photos/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoLibrary-Display-Photos
+//
+//  Created by Mohammad Azam on 2/17/19.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView! 
+    
+}
---
+++ PhotoLibrary-Display-Photos/PhotoLibrary-Display-Photos/PhotosCollectionViewController.swift
@@ -0,0 +1,81 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoLibrary-Display-Photos
+//
+//  Created by Mohammad Azam on 2/17/19.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    private var images = [PHAsset]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        populatePhotos()
+    }
+    
+    private func setupUI() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
+            fatalError("PhotoCollectionViewCell is not found")
+        }
+        
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+            
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+            
+        }
+        
+        return cell
+        
+    }
+    
+    private func populatePhotos() {
+        
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            
+            if status == .authorized {
+                
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects { (object,_, _) in
+                    self?.images.append(object)
+                }
+                
+                self?.images.reverse()
+                
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+                
+                
+            }
+            
+        }
+        
+    }
+    
+}
